656aabdf838db10b93b15dfbd9cf8318c7dc00bc
RapboyChenFW <64623378+RapboyChenFW@users.noreply.github.com>
2020-05-01 19:08:48 +0800

    Update 206_Reverse Linked List.java

--- 4.30/206_Reverse Linked List.java
+++ 4.30/206_Reverse Linked List.java
@@ -1,1 +1,12 @@
-
+class Solution {
+    public ListNode reverseList(ListNode head) {
+        ListNode new_head = null;
+        while(head!=null){
+            ListNode tmp = head;
+            head = head.next;
+            tmp.next = new_head;
+            new_head = tmp;
+        }
+        return new_head;
+    }
+}
